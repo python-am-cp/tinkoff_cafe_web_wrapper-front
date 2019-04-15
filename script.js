9e4af18a1058a3b69d00628af103b1b5475cd2a7
subelta <40162440+subelta@users.noreply.github.com>
2019-04-15 14:07:10 +0300

    change ngrok

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ var button = document.querySelector('.get-started');
 button.addEventListener("click", function() {
     var XHR = ("onload" in new XMLHttpRequest()) ? XMLHttpRequest : XDomainRequest;
     var xhr = new XHR();
-
-    xhr.open('GET', 'https://36f57679.ngrok.io', true);
+    
+    xhr.open('GET', 'http://2c8f55b6.ngrok.io', true);
 
     xhr.onload = function() {
         alert( this.responseText );
