228f2956561304713b385712f5ba478d3dc28588
subelta <40162440+subelta@users.noreply.github.com>
2019-04-15 14:45:10 +0300

    request_page

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ button.addEventListener("click", function() {
     var XHR = ("onload" in new XMLHttpRequest()) ? XMLHttpRequest : XDomainRequest;
     var xhr = new XHR();
     
-    xhr.open('GET', 'https://2c8f55b6.ngrok.io', true);
+    xhr.open('GET', 'https://2c8f55b6.ngrok.io/request_page', true);
 
     xhr.onload = function() {
         alert( this.responseText );
